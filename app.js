5c83bd06c84b2593c058545d86cbb5a526067375
brian727 <brian.w.727@gmail.com>
2021-07-08 10:47:00 -0700

    add'l js dev

--- app.js
+++ app.js
@@ -7,3 +7,31 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}              
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    console.log("user choice =>" + userChoice);
+    console.log("computer choice =>"  + computerChoice);
+}
+                       
+function main () {
+    rock_div.addEventListener('click', function() {
+        game("r");
+    })                                  
+    paper_div.addEventListener('click', function() {
+        game("p");
+    })
+    scissors_div.addEventListener('click', function() {
+        game("s");
+    })
+}
+
+main();
+
+
